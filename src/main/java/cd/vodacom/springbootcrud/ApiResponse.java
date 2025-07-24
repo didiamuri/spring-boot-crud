7b22bef2de24292c9ff6248a28352979c2621b73
Didierson Amuri <didiamuri@gmail.com>
2025-07-24 01:04:02 +0100

    addint ApiResponse, PersonController, PersonRepository, SecurityConfig

---
+++ src/main/java/cd/vodacom/springbootcrud/ApiResponse.java
@@ -0,0 +1,21 @@
+package cd.vodacom.springbootcrud;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ApiResponse<T> {
+    private int statusCode;
+    private String statusText;
+    private String message;
+    private T data;
+
+    public ApiResponse(int statusCode, String statusText, String message, T data) {
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+        this.message = message;
+        this.data = data;
+    }
+
+}
